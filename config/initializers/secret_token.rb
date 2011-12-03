6363e5bf3bbd2f3efb8e83b27263c2c273ff3de7
saikatu <saikatu.web@gmail.com>
2011-12-03 22:25:45 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yettodo::Application.config.secret_token = '0d887fdbf67dd3a81c9f559b711adb0686b29af70be8c504c5a9db70fdfd6a6457ef9293c155fe559ab209fdee649a0e5a17769bc7629bcf45aba9090647c6d2'
